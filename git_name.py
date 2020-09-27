886065e6c832a897ec63171f8917500aa08eef83
IT民工 <531461379@qq.com>
2020-09-27 18:09:18 +0800

    线路url

--- git_name.py
+++ git_name.py
@@ -2,6 +2,21 @@
 # -*- coding: utf-8 -*-
 # @Time : 2020/9/24 16:38
 
+import requests
+import time
+import json
+import os
+
+def getserver(url,url_config):
+    res = requests.post(url)
+    res_json = json.loads(res.text)
+    json_serverarealist = res_json.get("serverarealist")
+    for i in json_serverarealist:
+        servernames = i.get("serverareaname")
+        url_wanzheng = servernames+url_config
+        print(url_wanzheng)
 
 
-zhangyihua
+
+
+getserver("http://global.talk-cloud.net/ClientAPI/getserverarea","1.talk-cloud.net")
