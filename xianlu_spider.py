7c8e03e0c67c6048a7a29419eaf4578fb6170e50
IT民工 <531461379@qq.com>
2020-12-24 18:15:22 +0800

    v2更新

--- xianlu_spider.py
+++ xianlu_spider.py
@@ -4,6 +4,7 @@
 
 import requests
 import json,time
+import os
 
 
 class Spider():
@@ -12,30 +13,39 @@ class Spider():
         self.url1 = "https://{}.talk-cloud.net/ClientAPI/getconfig"
         self.url2 = "https://global.talk-cloud.net/ClientAPI/getserverarea"
         self.data = {"serial":"981527802","userrole":0,"coursename": None}
+        self.server = "2" #输入集群几
 
     def getserver(self):
         res = requests.get(self.url2)
         res_json = json.loads(res.text)
         json_serverarealist = res_json.get("serverarealist")
         coursename_list = []
+        msg = ""
         for i in json_serverarealist:
             servernames = i.get("serverareaname")
-            # print(servernames)
+            url_ping = servernames+"{}.talk-cloud.net".format(self.server)
+            re_replase = str(os.popen("ping -n 1 %s"%url_ping).read())
+            ip = re_replase[(re_replase.find("[") + 1):re_replase.find("]")]
+            msg += ip
+            msg += '\n'
+            with open("lines", 'w', encoding='utf-8') as f:
+                f.write(msg)
             coursename_list.append(servernames)
         return coursename_list
 
     def getconfig(self):
+        msg = ""
         for i in self.getserver():
             time.sleep(1)
             self.data['coursename'] = i
             res = requests.get(self.url1.format(i), self.data)
-            print(res.text)
             time.sleep(1)
             new_json = json.loads(res.text)
             newcour_seaddrs = new_json.get("newcourseaddr")[0]
             change = newcour_seaddrs.get("change")
             print(change)
 
-aaa = Spider()
-aaa.getconfig()
-aaa.getserver()
+if __name__ == '__main__':
+    aaa = Spider()
+    aaa.getserver()
+    aaa.getconfig()
